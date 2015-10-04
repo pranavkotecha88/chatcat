42e80293e0fb36d9bbfe2076bcd73389d7d65950
Pranav Kotecha <pranavkotecha88@gmail.com>
2015-10-04 13:37:08 -0700

    Added UsersList logic

--- socket/socket.js
+++ socket/socket.js
@@ -18,10 +18,27 @@ module.exports = function(io, rooms) {
 			socket.username = data.user;
 			socket.userPic = data.userPic;
 			socket.join(data.room);
+			updateUserList(data.room, true);
 		})
 
 		socket.on('newMessage', function(data) {
 			socket.broadcast.to(data.room_number).emit('messagefeed', JSON.stringify(data));
+		})
+
+		function updateUserList(room, updateAll) {
+			var getUsers = io.of('/messages').clients(room);
+			var userList = [];
+			for(var i in getUsers) {
+				userList.push({user:getUsers[i].username, userPic:getUsers[i].userPic});
+			}
+			socket.to(room).emit('updateUsersList', JSON.stringify(userList));
+			if(updateAll) {
+				socket.broadcast.to(room).emit('updateUsersList', JSON.stringify(userList));
+			}
+		}
+
+		socket.on('updateList', function(data){
+			updateUserList(data.room);
 		})
 	})
 }
